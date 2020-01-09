3e03ebbf62ae0a249bce8a627218916a4c96ae56
Kristedd <kristiangerkman1@gmail.com>
2020-01-09 19:46:18 +0200

    Updates

---
+++ precache-manifest.6874ff8c95dcfa70f937126f2545e117.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d0b7fe1a16e4ea5a289cd43920659d8",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "d31b9d7be89bf134e678",
+    "url": "/cv/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5a0475895783c9aee240",
+    "url": "/cv/static/js/2.0e80b381.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cv/static/js/2.0e80b381.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d31b9d7be89bf134e678",
+    "url": "/cv/static/js/main.0ddefb87.chunk.js"
+  },
+  {
+    "revision": "babaf6e92ae8ceb9554c",
+    "url": "/cv/static/js/runtime-main.ebfb0654.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cv/static/media/logo.5d5d9eef.svg"
+  }
+]);
