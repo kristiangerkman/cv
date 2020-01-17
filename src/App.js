48d3a11d5d58d60f960b49c2bc7eefba398a603f
Kristedd <kristiangerkman1@gmail.com>
2020-01-17 19:03:15 +0200

    add cv file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import pdf from "./Gerkman_Kristian_CV.pdf"
 
 function App() {
   return (
     <div className="App">
-   <h1> Mr. Gerkman </h1> 
+      <h1> Mr. Gerkman </h1>
+
+      <embed src={pdf} width="1000" height="1200" />
     </div>
   );
 }
